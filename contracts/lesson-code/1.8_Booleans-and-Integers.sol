f90003cc411621b30447a23e330191a96dea48b4
dd0sxx <contacttheotelonis@gmail.com>
2022-05-17 11:59:32 -0400

    bools and ints

---
+++ contracts/lesson-code/1.8_Booleans-and-Integers.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.8.13;
+
+contract Space {
+
+    // Boolean Type - default false
+    bool public sold;
+
+    // Integer Type (uint / int = uint256 / int256 )  - default 0
+    uint public price;
+    int public location;
+}
