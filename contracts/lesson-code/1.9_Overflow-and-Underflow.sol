2c2cee2053f6b75d30d0c8ab2454781e6f6494da
dd0sxx <contacttheotelonis@gmail.com>
2022-05-18 12:55:11 -0400

    overflow and underflow

---
+++ contracts/lesson-code/1.9_Overflow-and-Underflow.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.13;
+
+contract Space {
+
+    bool public sold;
+
+    // uint8 initialized with the maximum value for the type
+    uint8 public price = 255;
+    int8 public location = -10;
+
+    // the first call to f1() will return 0
+    function f1 () public returns (uint8) {
+        unchecked {
+        price += 1;
+        }
+    }
+
+    // will revert since solc version ^0.8.0 has built in overflow/underflow checks
+    function f2 () public returns (uint8) {
+        price += 1;
+    }
+}
