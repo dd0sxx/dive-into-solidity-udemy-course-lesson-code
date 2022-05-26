c32879fe68910652cc06ceb30dcfd2d79ad63758
dd0sxx <contacttheotelonis@gmail.com>
2022-05-26 13:59:31 -0600

    payable receive sned ether

---
+++ contracts/lesson-code/1.19_Payable-Receive-Fallback.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.13;
+
+contract Deposit {
+
+    modifier onlyOwner {
+        require(msg.sender == owner);
+        _; 
+    }
+
+    address public owner;
+
+    constructor () {
+        owner = msg.sender
+    }
+
+    receive () external payable {
+        
+    }
+
+    fallback () external payable {
+
+    }
+
+    function getBalance () public view returns (uint) {
+        return address(this).balance;
+    }
+
+    function sendEther (address payable destination, uint amount) public onlyOwner returns (bool) {
+        ///@dev returns false on failure
+        bool res = destination.send(amount);
+        return res;
+    }
+
+    function transferEther (address payable destination, uint amount) onlyOwner public {
+        ///@dev reverts on failure
+        destination.transfer(amount);
+    }
+
+    function callEther (address payable destination, uint amount) onlyOwner public {
+        ///@dev note, this call is dangerous and allows for reentrancy
+        destination.call{value: amount}("");
+    }
+}
