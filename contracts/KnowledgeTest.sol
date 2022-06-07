b467a9b00f7cc904808952b29b2e5bc356d7c2ed
Gab <gabrieljim641@gmail.com>
2022-06-07 15:30:59 +0200

    Adds new exercise's solution

---
+++ contracts/KnowledgeTest.sol
@@ -0,0 +1,42 @@
+//SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+contract KnowledgeTest {
+    string[] public tokens = ["BTC", "ETH"];
+    address[] public players;
+    address public owner;
+
+    receive() external payable {}
+
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
+
+    function transferAll(address account) external {
+        require(msg.sender == owner, "ONLY_OWNER");
+        (bool result, ) = account.call{value: getBalance()}("");
+        require(result, "TX_FAILED");
+    }
+
+    function start() external {
+        players.push(msg.sender);
+    }
+
+    function concatenate(string calldata string1, string calldata string2)
+        external
+        pure
+        returns (string memory)
+    {
+        return string(abi.encodePacked(string1, string2));
+    }
+
+    function changeTokens() external {
+        string[] storage t = tokens;
+        t[0] = "VET";
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+}
